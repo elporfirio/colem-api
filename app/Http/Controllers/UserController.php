778b34db431cdbf1ac118e76da4823cd6eb58342
Porfirio Chavez <porfirio.chavez@iPorfirio.local>
2017-05-05 14:01:47 -0500

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: porfirio.chavez
+ * Date: 05/05/17
+ * Time: 11:11
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class UserController extends Controller
+{
+    private $user;
+
+    public function show($id = null){
+
+        if($id){
+            $this->user = User::find($id);
+        } else {
+            $this->user = User::all();
+        }
+
+        return response()->json($this->user);
+    }
+
+    public function create(Request $request){
+        $this->validate($request, [
+            'username' => 'required|unique:users|max:255',
+            'email' => 'required|unique:users',
+        ]);
+
+        $this->user = User::create($request->all());
+//        var_dump($id);
+//        var_dump($request->query());
+//        var_dump($request->input('hola'));
+
+        return response()->json($this->user);
+    }
+}
---
+++ app/Models/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
