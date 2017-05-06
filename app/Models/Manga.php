0a42a6f6626ed0779399c4fb5462e3397c58a4cc
Porfirio Chavez Gonzalez <elporfirio@iMac-de-Porfirio.local>
2017-05-05 21:37:13 -0500

    Creating Models

---
+++ app/Models/Manga.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elporfirio
+ * Date: 05/05/17
+ * Time: 8:38 PM
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Manga extends Model
+{
+
+}
---
+++ database/migrations/2017_05_06_0004_create_mangas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMangasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mangas', function(Blueprint $table){
+           $table->increments('id');
+           $table->string('title');
+           $table->date('publishedAt');
+           $table->integer('volume');
+           $table->float('price');
+           $table->string('isbn');
+           $table->integer('serie_id')->unsigned();
+           $table->foreign('serie_id')->references('id')->on('series');
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mangas');
+    }
+}
