3ba4c8eec1b72d9efba6ccd2094109d87036b939
Porfirio Chavez <porfirio.chavez@iPorfirio.local>
2017-05-09 12:29:48 -0500

    implement users CRUD

---
+++ app/Http/Controllers/MangaController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: porfirio.chavez
+ * Date: 09/05/17
+ * Time: 11:35
+ */
+
+namespace App\Http\Controllers;
+
+
+class MangaController
+{
+
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
+    /**
+     * @var
+     */
     private $user;
 
+    /**
+     * @param null $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($id = null){
 
         if($id){
@@ -27,6 +37,10 @@ class UserController extends Controller
         return response()->json($this->user);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function create(Request $request){
         $this->validate($request, [
             'username' => 'required|unique:users|max:255',
@@ -41,11 +55,48 @@ class UserController extends Controller
         return response()->json($this->user);
     }
 
-    public function testManga(Request $request){
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id){
+        $this->validate($request, [
+            'username' => 'required|max:255|unique:users,username,' . $id,
+            'email' => 'required|unique:users,email,' . $id,
+        ]);
 
-        $user = User::with('mangas')->find(1);
+        $this->user = User::find($id);
+        $this->user->username = $request->input('username');
+        $this->user->email = $request->input('email');
+        $this->user->save();
+
+        return response()->json($this->user);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(Request $request, $id){
+        $this->user = User::find($id);
+        $this->user->delete();
+
+        return response()->json($this->user);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function testManga(Request $request, $id){
+
+        $this->user = User::with('mangas')->find($id);
+
         //$user->mangas()->attach(1);
 
-        return response()->json($user);
+        return response()->json($this->user);
     }
 }
